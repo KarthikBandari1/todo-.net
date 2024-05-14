ba9b37b079a3610afe7c258c07a2b3b61dfc585a
karthik <karthik@gmail.com>
2024-05-15 02:54:10 +0530

    first commit

---
+++ Data/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTodo.Models;
+
+namespace MyTodo.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
